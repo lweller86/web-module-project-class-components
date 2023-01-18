c63189a127f91a00bf19e6ba19520c190c795e5f
lweller86 <112785242+lweller86@users.noreply.github.com>
2023-01-18 11:32:08 -0500

    todo list rendered and working with task completed

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,14 +1,36 @@
 import React from 'react'
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos:  [
+        {
+          task: 'Organize Garage',
+          id: 1528817077286, 
+          completed: false
+        },
+        {
+          task: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+      ]
+    }
+  }
+
+
   render() {
+    const { todos } = this.state
     return (
       <div>
         <h1>ToDo App</h1>
         <ul>
-          <li>Walk Dog</li>
-          <li>Work Out</li>
-          <li>Take out Trash</li>
+          {
+            todos.map(todo => {
+              return (<li>{todo.task} { todo.completed? <span>Completed</span> : <span></span>}</li>)
+            })
+          }
         </ul>
         <form>
           <input/>
